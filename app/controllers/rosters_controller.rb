4c508c84fcd38ef145a59f37bade386870c22492
Alexander Blythe <Alexander.blythe@gmail.com>
2015-05-28 15:58:00 -0400

    Players broken out by detailed positions

--- app/controllers/rosters_controller.rb
+++ app/controllers/rosters_controller.rb
@@ -14,23 +14,26 @@ class RostersController < ApplicationController
     roster_major = @user.rosters.find_by(forty_five: true)
     @pitchers = roster_major.players.where(position: "P").order(sort)
     @catchers = roster_major.players.where(position: "C").order(sort)
-    @infielders = roster_major.players.where("position LIKE ?", "%B").order(sort)
-    ss = roster_major.players.where(position: "SS").order(sort)
-    ss.each do |p|
-      @infielders.push p 
-    end
-    @outfielders = roster_major.players.where("position LIKE ?", "%F").order(sort)
+    @first = roster_major.players.where(position: "1B").order(sort)
+    @second = roster_major.players.where(position: "2B").order(sort)
+    @third = roster_major.players.where(position: "3B").order(sort)
+    @ss = roster_major.players.where(position: "SS").order(sort)
+    @right = roster_major.players.where(position: "RF").order(sort)
+    @left = roster_major.players.where(position: "LF").order(sort)
+    @center =roster_major.players.where(position: "CF").order(sort)
+    
     @dhs = roster_major.players.where(position: "DH").order(sort)
     
     roster_minor =@user.rosters.find_by(minor: true)
     @pitchers_minor = roster_minor.players.where(position: "P").order(sort)
     @catchers_minor = roster_minor.players.where(position: "C").order(sort)
-    @infielders_minor = roster_minor.players.where("position LIKE ?", "%B").order(sort)
-    ss_minors = roster_minor.players.where(position: "SS").order(sort)
-    ss_minors.each do |p|
-      @infielders_minor.push p 
-    end
-    @outfielders_minor = roster_minor.players.where("position LIKE ?", "%F").order(sort)
+    @first_minor = roster_minor.players.where(position: "1B").order(sort)
+    @second_minor = roster_minor.players.where(position: "2B").order(sort)
+    @third_minor = roster_minor.players.where(position: "3B").order(sort)
+    @ss_minor = roster_minor.players.where(position: "SS").order(sort)
+    @left_minor = roster_minor.players.where(position: "LF").order(sort)
+    @right_minor = roster_minor.players.where(position: "RF").order(sort)
+    @center_minor = roster_minor.players.where(position: "CF").order(sort)
     @dhs_minor = roster_minor.players.where(position: "DH").order(sort)
   end
 end
